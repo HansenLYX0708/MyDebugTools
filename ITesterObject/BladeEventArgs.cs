7831f96a3a420243e55f86593440669d6e400398
HansenLYX0708 <1195857096@qq.com>
2018-06-07 16:40:24 +0800

    Add project files.

---
+++ ITesterObject/BladeEventArgs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hitachi.Tester.Module
+{
+    public class BladeEventArgs : EventArgs
+    {
+        #region Fields
+
+        #endregion Fields
+
+        #region Constructors
+
+        #endregion Constructors
+
+        #region Properties
+
+        #endregion Properties
+
+        #region Methods
+
+        #endregion Methods
+    }
+
+    public class CompletedEventBladeArgs : BladeEventArgs
+    {
+
+    }
+
+    public class SequenceStartedBladeEventArgs : BladeEventArgs
+    {
+
+    }
+
+    public class StatusEventBladeArgs : BladeEventArgs
+    {
+
+    }
+
+
+}
+
---
+++ TesterObject/TesterObject.SendEvent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+using Hitachi.Tester.Enums;
+
+namespace Hitachi.Tester.Module
+{
+    public partial class TesterObject
+    {
+        #region Fields
+        private Queue<BladeEventStruct> bladeEventQueue = new Queue<BladeEventStruct>();
+        private ReaderWriterLock bladeEventQueueLock = new ReaderWriterLock();
+        private Thread bladeEventsThread;
+        private object bladeEventLockObj = new object();
+
+        private delegate void AsyncSendGenericEventDelegate(object sender, StatusEventArgs e, ref StatusEventHandler handler, Queue<BladeEventArgs> queue, string eventName);
+        private MemsStateValues lastBunnyStatusSent = MemsStateValues.Unknown;
+        #endregion Fields
+
+        #region Properties
+
+        #endregion Properties
+
+        #region Methods
+        /// <summary>
+        /// Puts items in the queue
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SendBladeEventCallback(object sender, BladeEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// This function pulls items out and sends to remote clients.
+        /// Actually it calls the callback function in each client via proxy.
+        /// </summary>
+        private void doBladeEvents()
+        {
+
+        }
+        #endregion Methods
+    }
+}
